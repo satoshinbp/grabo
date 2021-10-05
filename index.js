57cd26d567005a030e020f0eb0f6560760763855
Shinya Sato <0113shin.s@gmail.com>
2021-10-05 15:25:12 -0700

    connect mongodb

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const app = express();
-const port = 8000;
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 8000
+const uri = process.env.MONGO_URI
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log('connected to db'))
+  .catch((err) => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
+  console.log(`Example app listening on port ${port}!`)
+})
