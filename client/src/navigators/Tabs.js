816a1babad85ff8f57a0e7a254763750841426c6
Shinya Sato <0113shin.s@gmail.com>
2021-10-06 11:44:18 -0700

    hide tab top header

--- client/src/navigators/Tabs.js
+++ client/src/navigators/Tabs.js
@@ -13,11 +13,11 @@ export default () => {
   return (
     <>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeStacks} />
-        <Tab.Screen name="Groups" component={GroupsStacks} />
-        <Tab.Screen name="Scan" component={ScanStacks} />
-        <Tab.Screen name="Favs" component={FavsStacks} />
-        <Tab.Screen name="Profile" component={ProfileStacks} />
+        <Tab.Screen name="Home" component={HomeStacks} options={{ headerShown: false }} />
+        <Tab.Screen name="Groups" component={GroupsStacks} options={{ headerShown: false }} />
+        <Tab.Screen name="Scan" component={ScanStacks} options={{ headerShown: false }} />
+        <Tab.Screen name="Favs" component={FavsStacks} options={{ headerShown: false }} />
+        <Tab.Screen name="Profile" component={ProfileStacks} options={{ headerShown: false }} />
       </Tab.Navigator>
     </>
   )
