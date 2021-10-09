82a84cbc36f4a1238a5cb248f2a9cb56393674f4
Shinya Sato <0113shin.s@gmail.com>
2021-10-08 17:46:36 -0700

    fix screen wording

--- client/src/navigators/Tabs.js
+++ client/src/navigators/Tabs.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { useSelector } from 'react-redux'
-import HomeStacks from './HomeStacks'
+import ProductsStacks from './ProductsStacks'
 import GroupsStacks from './GroupsStacks'
 import ScanStacks from './ScanStacks'
 import FavsStacks from './FavsStacks'
@@ -12,10 +11,10 @@ const Tab = createBottomTabNavigator()
 export default () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeStacks} options={{ headerShown: false }} />
-      <Tab.Screen name="Groups" component={GroupsStacks} options={{ headerShown: false }} />
+      <Tab.Screen name="My Products" component={ProductsStacks} options={{ headerShown: false }} />
+      <Tab.Screen name="My Groups" component={GroupsStacks} options={{ headerShown: false }} />
       <Tab.Screen name="Scan" component={ScanStacks} options={{ headerShown: false }} />
-      <Tab.Screen name="Favs" component={FavsStacks} options={{ headerShown: false }} />
+      <Tab.Screen name="Favorites" component={FavsStacks} options={{ headerShown: false }} />
       <Tab.Screen name="Profile" component={ProfileStacks} options={{ headerShown: false }} />
     </Tab.Navigator>
   )
