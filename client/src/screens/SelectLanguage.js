a68b2eb994973aae436901350570c0955605dc94
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-10-18 10:51:32 -0700

    separate LanguageGroups component

--- client/src/screens/SelectLanguage.js
+++ client/src/screens/SelectLanguage.js
@@ -1,24 +1,6 @@
 import React, { useState } from 'react'
-import { Radio, Center, NativeBaseProvider, Box, Heading, Text } from 'native-base'
-import groups from '../utils/groups'
-
-export const LanguageGroups = () => {
-  const [value, setValue] = useState('')
-
-  return (
-    <Radio.Group
-      name="Group"
-      value={value}
-      onChange={(nextValue) => {
-        setValue(nextValue)
-      }}
-    >
-      {groups.map((language) => (
-        <Radio value={language.code}>{language.language}</Radio>
-      ))}
-    </Radio.Group>
-  )
-}
+import { Center, Box, Heading, Text } from 'native-base'
+import LanguageGroups from '../components/LanguageGroups'
 
 export default () => {
   return (
@@ -27,11 +9,9 @@ export default () => {
         <Heading>Select Language</Heading>
         <Text>Create a new product by selecting a language from the list below</Text>
       </Box>
-      <NativeBaseProvider>
-        <Center flex={1} px="3">
-          <LanguageGroups />
-        </Center>
-      </NativeBaseProvider>
+      <Center flex={1} px="3">
+        <LanguageGroups />
+      </Center>
     </>
   )
 }
