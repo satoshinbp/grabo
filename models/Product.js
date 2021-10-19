05b2ff86aab2ae2bdf4bf5b4e446499610c2b447
Shinya Sato <0113shin.s@gmail.com>
2021-10-19 09:38:28 -0700

    rename schema keys

--- models/Product.js
+++ models/Product.js
@@ -4,35 +4,42 @@ const ObjectId = Schema.Types.ObjectId
 
 const productSchema = new Schema(
   {
-    img: {
-      url: { type: [String], required: true },
-      report: { wrong: Number, affiliate: Number, threats: Number, privacy: Number },
-    },
-    lang: String,
+    userId: ObjectId,
+    images: [
+      {
+        url: ObjectId,
+        report: { wrong: Number, affiliate: Number, threats: Number, privacy: Number },
+      },
+    ],
+    group: String,
     keywords: [String],
-    fixedQandA: [
+    fixedQandAs: [
       {
         question: {
-          title: { type: String, required: true },
+          description: String,
           report: { wrong: Number, affiliate: Number, threats: Number, privacy: Number },
         },
         answers: [
           {
-            userId: { type: String, required: true },
-            value: { type: String, required: true },
+            userId: ObjectId,
+            description: String,
             report: { wrong: Number, affiliate: Number, threats: Number, privacy: Number },
           },
         ],
         highlightedBy: [String],
       },
     ],
-    uniqueQandA: [
+    uniqQandAs: [
       {
-        question: { userId: { type: String, required: true }, value: { type: String, required: true } },
+        question: {
+          userId: ObjectId,
+          description: String,
+          report: { wrong: Number, affiliate: Number, threats: Number, privacy: Number },
+        },
         answers: [
           {
-            userId: { type: String, required: true },
-            value: { type: String, required: true },
+            userId: ObjectId,
+            description: String,
             report: { wrong: Number, affiliate: Number, threats: Number, privacy: Number },
           },
         ],
