4e66fcadde10f024f3b8e636014211e5503ec77a
Nori <loveandfight2186@yahoo.co.jp>
2021-10-19 17:55:55 +0900

    create CreateProduct component and add example of s3 upload

---
+++ client/src/screens/CreateProduct.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Image } from 'react-native'
+import { View, VStack, Divider, Button } from 'native-base'
+import axios from 'axios'
+import * as ImagePicker from 'expo-image-picker'
+import { Box, Heading } from 'native-base'
+
+export const CreateProduct = () => {
+  const navigation = useNavigation()
+  const [image, setImage] = useState(null)
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    })
+
+    if (!result.cancelled) {
+      setImage(result.uri)
+    }
+  }
+
+  const uploadImage = () => {
+    let params = new FormData()
+    params.append('image', { uri: image.replace('file://', ''), name: 'uploadedImage.jpeg', type: 'image/jpeg' })
+    axios
+      .post('http://"your IP":8000/api/image', params, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <View h="100%" flex={1} px={4} bg="#fff">
+      <Box>
+        <Heading>My Products</Heading>
+      </Box>
+      <VStack alignItems="center" space={4} w="100%" h="100%" mt={4} divider={<Divider />}>
+        <Button onPress={pickImage} w="100%">
+          Choose pic
+        </Button>
+        <Button onPress={uploadImage} w="100%">
+          upload
+        </Button>
+        {/* display selected image */}
+        <Image source={{ uri: image }} alt="image" style={{ width: 300, height: 300 }} />
+        {/* example of fetched image from S3 */}
+        <Image
+          source={{ uri: 'https://grabo1.s3.amazonaws.com/1634460715953' }}
+          alt="image"
+          style={{ width: 300, height: 300 }}
+        />
+      </VStack>
+    </View>
+  )
+}
