68de5c51c9bd5598bccfab7fb34421aa3c6b1402
Nori <loveandfight2186@yahoo.co.jp>
2021-10-23 02:57:40 +0900

    func: add constraint that user need to belong to at least one group

--- client/src/screens/SettingGroup.js
+++ client/src/screens/SettingGroup.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { VStack, HStack, Checkbox, Box, Heading, Button } from 'native-base'
+import { Text, VStack, HStack, Checkbox, Box, Heading, Button } from 'native-base'
 import grouplists from '../utils/groups'
 import { updateGroup } from '../features/user'
 
 export default () => {
   const [groups, setGroups] = useState([])
   const user = useSelector((state) => state.user.value)
+  const [isError, setIsError] = useState(false)
   const dispatch = useDispatch()
+
+  const saveButtonHandling = () => {
+    if (groups.length === 0) {
+      return setIsError(true)
+    }
+    setIsError(false)
+    dispatch(updateGroup(groups))
+  }
+
   return (
     <Box>
       <VStack space={2}>
@@ -30,7 +40,8 @@ export default () => {
           ))}
         </Checkbox.Group>
       </VStack>
-      <Button onPress={() => dispatch(updateGroup(groups))}>Save</Button>
+      {isError && <Text> You have to belong to at least one Group</Text>}
+      <Button onPress={saveButtonHandling}>Save</Button>
     </Box>
   )
 }
