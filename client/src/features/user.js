f2298fcb36a2bc0f8891eba68aa3f3b2649425f0
Shinya Sato <0113shin.s@gmail.com>
2021-10-30 10:30:12 -0700

    add clearing token process in logout

--- client/src/features/user.js
+++ client/src/features/user.js
@@ -5,15 +5,15 @@ import { SERVER_ROOT_URI } from '@env'
 // SERVER_ROOT_URI might not work depends on dev environment
 // In that case, replace SERVER_ROOT_URI to "<your network IP address>:<PORT>""
 
-export const login = createAsyncThunk('users/signin', async (idToken, thunkAPI) => {
-  try {
-    const res = await axios.post(`${SERVER_ROOT_URI}/auth/google`, { idToken })
-    const { token, user } = res.data
-    await SecureStore.setItemAsync('token', token)
-    return user
-  } catch (err) {
-    throw err
-  }
+export const login = createAsyncThunk('users/login', async (idToken) => {
+  const res = await axios.post(`${SERVER_ROOT_URI}/auth/google`, { idToken })
+  const { token, user } = res.data
+  await SecureStore.setItemAsync('token', token)
+  return user
+})
+
+export const logout = createAsyncThunk('users/logout', async () => {
+  await SecureStore.deleteItemAsync('token')
 })
 
 const initialUserState = {
@@ -30,9 +30,6 @@ const userSlice = createSlice({
   name: 'user',
   initialState: { user: initialUserState, loading: false },
   reducers: {
-    logout: (state, action) => {
-      state.user = initialUserState
-    },
     updateGroup: (state, action) => {
       state.user = { ...state.user, groups: action.payload }
     },
@@ -48,8 +45,18 @@ const userSlice = createSlice({
     [login.rejected]: (state, action) => {
       state.loading = false
     },
+    [login.pending]: (state, action) => {
+      state.loading = true
+    },
+    [login.fulfilled]: (state, action) => {
+      state.user = initialUserState
+      state.loading = false
+    },
+    [login.rejected]: (state, action) => {
+      state.loading = false
+    },
   },
 })
 
-export const { logout, updateGroup } = userSlice.actions
+export const { updateGroup } = userSlice.actions
 export default userSlice.reducer
