447c6ca1765f8d741cb074a43e08e108c30548d9
Shinya Sato <0113shin.s@gmail.com>
2021-11-12 10:48:48 -0800

    temp style

--- client/src/screens/Product.js
+++ client/src/screens/Product.js
@@ -1,7 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useRoute } from '@react-navigation/core'
-import { View, FlatList, Image, Text, Button, Divider, Input, Center, Accordion, VStack } from 'native-base'
+import { useRoute, useNavigation } from '@react-navigation/core'
+import { Dimensions } from 'react-native'
+import {
+  View,
+  ScrollView,
+  Box,
+  VStack,
+  HStack,
+  Center,
+  Pressable,
+  Accordion,
+  Image,
+  Text,
+  Button,
+  Divider,
+  Input,
+} from 'native-base'
 import Carousel, { Pagination } from 'react-native-snap-carousel'
 import { setProduct } from '../features/product'
 import { addAnswer, addUniqQuestion, updateHighlight } from '../api/product'
@@ -9,12 +24,16 @@ import Loading from '../components/Loading'
 import ProductActionModal from '../components/ProductActionModal'
 import Report from '../components/Report'
 
-export default ({ navigation }) => {
+const windowWidth = Dimensions.get('window').width
+
+export default () => {
   const route = useRoute()
+  const navigation = useNavigation()
+
   const { token, user } = useSelector((state) => state.auth)
   const { product, loading } = useSelector((state) => state.product)
   const dispatch = useDispatch()
-  const [questionIndex, setQuestionIndex] = useState('')
+
   const [activeSlide, setActiveSlide] = useState(0)
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [reportItem, setReportItem] = useState(null)
@@ -31,6 +50,7 @@ export default ({ navigation }) => {
 
   const handleAnswerSubmit = async () => {
     const params = { id: product._id, answer }
+    console.log(answer)
     try {
       await addAnswer(token, params)
       setAnswer({})
@@ -64,233 +84,145 @@ export default ({ navigation }) => {
 
   const carouselImages = ({ item }) => <Image source={{ uri: item.url }} alt="product image" size="100%" />
 
-  const PaginationComponent = (images) => {
-    return (
-      <View>
-        <Pagination
-          dotsLength={images.length}
-          activeDotIndex={activeSlide}
-          containerStyle={{ backgroundColor: 'rgba(255, 255, 255)' }}
-          alignSelf="center"
-          dotStyle={{
-            width: 10,
-            height: 10,
-            borderRadius: 5,
-            backgroundColor: 'rgba(0, 0, 0, 0.54)',
+  const PaginationComponent = (images) => (
+    <View>
+      <Pagination
+        dotsLength={images.length}
+        activeDotIndex={activeSlide}
+        containerStyle={{ backgroundColor: 'rgba(255, 255, 255)' }}
+        alignSelf="center"
+        dotStyle={{
+          width: 10,
+          height: 10,
+          borderRadius: 5,
+          backgroundColor: 'rgba(0, 0, 0, 0.54)',
+        }}
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+      />
+      <Center w={windowWidth} />
+    </View>
+  )
+
+  const QandAAccordions = (QandAs, type) =>
+    QandAs.map((QandA, index) => (
+      <>
+        <Accordion>
+          <Accordion.Item>
+            <Accordion.Summary>
+              <HStack alignItems="center">
+                <VStack flex={1}>
+                  <Text>{type === 'uniq' ? QandA.question.description : QandA.question}</Text>
+                  <Text fontSize="xs">
+                    This question has&nbsp;
+                    {QandA.answers.length}
+                    {QandA.answers.length > 1 ? ' answers' : ' answer'}
+                  </Text>
+                </VStack>
+                <Pressable
+                  onPress={() => {
+                    const highlightStatus = QandA.highlightedBy.includes(user._id)
+                    const params = {
+                      id: product._id,
+                      userId: user._id,
+                      isUniqQuestion: true,
+                      questionIndex: index,
+                      isHighlighted: highlightStatus,
+                    }
+                    handleHighlightSubmit(params)
+                  }}
+                >
+                  <Box>{`★ ${QandA.highlightedBy.length}`}</Box>
+                </Pressable>
+                <Accordion.Icon />
+              </HStack>
+            </Accordion.Summary>
+            <Accordion.Details
+              margin={0}
+              padding={0}
+              backgroundColor="linear-gradient(180deg, rgba(255, 200, 20, 0.52) 0%, rgba(255, 255, 255, 0.8) 85.42%);"
+            >
+              {QandA.answers.map((answer, i) => (
+                <>
+                  <View padding={4} flexDirection="row" justifyContent="space-between">
+                    <Text>{answer.description}</Text>
+                    <Report modalHandler={() => modalHandler(QandA, index, i)} isModalOpen={isModalOpen} />
+                  </View>
+                  <Divider w="100%" />
+                </>
+              ))}
+            </Accordion.Details>
+          </Accordion.Item>
+        </Accordion>
+        <Input
+          placeholder="Please write an answer here"
+          blurOnSubmit={true}
+          returnKeyType="done"
+          onSubmitEditing={() => Keyboard.dismiss()}
+          value={answer}
+          onChangeText={(text) => {
+            setAnswer({
+              answer: {
+                userId: user._id,
+                description: text,
+              },
+              isUniqQuestion: true,
+              questionIndex: index,
+            })
           }}
-          inactiveDotOpacity={0.4}
-          inactiveDotScale={0.6}
+          alignItems="center"
         />
-        <Center
-          width={{
-            base: 400,
-            lg: 600,
-          }}
-        ></Center>
-      </View>
-    )
-  }
+        <Button onPress={handleAnswerSubmit}>Answer</Button>
+        <Divider w="100%" my={4} />
+      </>
+    ))
 
   if (loading) return <Loading />
   return (
     <>
-      <Carousel
-        data={product.images}
-        renderItem={carouselImages}
-        itemWidth={450}
-        sliderWidth={450}
-        onSnapToItem={(index) => setActiveSlide(index)}
-      />
-      <Text>{product.images ? PaginationComponent(product.images) : <Loading />}</Text>
-      <FlatList
-        data={product.uniqQandAs}
-        renderItem={({ item, index }) => (
-          <>
-            <Accordion>
-              <Accordion.Item>
-                <Accordion.Summary>
-                  <VStack>
-                    <Text>{item.question.description}</Text>
-                    <Text>
-                      This question has&nbsp;
-                      {item.answers.length}
-                      {item.answers.length > 1 ? ' answers' : ' answer'}
-                    </Text>
-                  </VStack>
-                  <Text textAlign={'right'}>Answer</Text>
-                  <Accordion.Icon />
-                </Accordion.Summary>
-                <Accordion.Details
-                  margin="0px"
-                  padding="0px"
-                  backgroundColor="linear-gradient(180deg, rgba(255, 200, 20, 0.52) 0%, rgba(255, 255, 255, 0.8) 85.42%);"
-                >
-                  {item.answers.map((answer, i) => (
-                    <>
-                      <View padding={4} flexDirection="row" justifyContent="space-between">
-                        <Text>{answer.description}</Text>
-                        <Report modalHandler={() => modalHandler(item, index, i)} isModalOpen={isModalOpen} />
-                      </View>
-                      <Divider w="100%" />
-                    </>
-                  ))}
-                </Accordion.Details>
-              </Accordion.Item>
-            </Accordion>
-            <Button
-              onPress={() => {
-                const highlightStatus = item.highlightedBy.includes(user._id)
-                const params = {
-                  id: product._id,
-                  userId: user._id,
-                  isUniqQuestion: true,
-                  questionIndex: index,
-                  isHighlighted: highlightStatus,
-                }
-                handleHighlightSubmit(params)
-              }}
-            >
-              ★{item.highlightedBy.length}
-            </Button>
-            <Input
-              mb="10"
-              placeholder="Please write an answer here"
-              blurOnSubmit={true}
-              returnKeyType="done"
-              onSubmitEditing={() => {
-                Keyboard.dismiss()
-              }}
-              alignItems="center"
-              value={answer}
-              onChangeText={(text) => {
-                setAnswer({
-                  answer: {
-                    userId: user._id,
-                    description: text,
-                  },
-                  isUniqQuestion: true,
-                  questionIndex: index,
-                })
-              }}
-            />
-            <Button onPress={handleAnswerSubmit}>Submit Answer</Button>
-            <Divider my={2} w="100%" />
-          </>
-        )}
-        keyExtractor={(item) => item.question}
-        showsVerticalScrollIndicator={false}
-      />
-      <FlatList
-        data={product.fixedQandAs}
-        renderItem={({ item, index }) => (
-          <>
-            <Accordion>
-              <Accordion.Item>
-                <Accordion.Summary>
-                  <VStack>
-                    <Text>{item.question}</Text>
-                    <Text>
-                      This question has&nbsp;
-                      {item.answers.length}
-                      {item.answers.length > 1 ? ' answers' : ' answer'}
-                    </Text>
-                  </VStack>
-                  <Text textAlign={'right'}>Answer</Text>
-                  <Accordion.Icon />
-                </Accordion.Summary>
-                <Accordion.Details
-                  margin={0}
-                  padding={0}
-                  backgroundColor="linear-gradient(180deg, rgba(255, 200, 20, 0.52) 0%, rgba(255, 255, 255, 0.8) 85.42%);"
-                >
-                  {item.answers.map((answer, i) => (
-                    <>
-                      <View padding={4} flexDirection="row" justifyContent="space-between">
-                        <Text>{answer.description}</Text>
-                        <Report modalHandler={() => modalHandler(item, index, i)} isModalOpen={isModalOpen} />
-                      </View>
-                      <Divider w="100%" />
-                    </>
-                  ))}
-                </Accordion.Details>
-              </Accordion.Item>
-            </Accordion>
-            <Button
-              onPress={() => {
-                const highlightStatus = item.highlightedBy.includes(user._id)
-                const params = {
-                  id: product._id,
-                  userId: user._id,
-                  isUniqQuestion: false,
-                  questionIndex: index,
-                  isHighlighted: highlightStatus,
-                }
-                handleHighlightSubmit(params)
-              }}
-            >
-              ★{item.highlightedBy.length}
-            </Button>
-            <Input
-              mb="10"
-              placeholder="Please write an answer here"
-              blurOnSubmit={true}
-              returnKeyType="done"
-              onSubmitEditing={() => {
-                Keyboard.dismiss()
-              }}
-              alignItems="center"
-              value={answer}
-              onChangeText={(text) => {
-                setAnswer({
-                  answer: {
-                    userId: user._id,
-                    description: text,
-                  },
-                  isUniqQuestion: false,
-                  questionIndex: index,
-                })
-              }}
-            />
-            <Button onPress={handleAnswerSubmit}>Submit Answer</Button>
-            <Divider my={2} w="100%" />
-          </>
-        )}
-        keyExtractor={(item) => item.question}
-        showsVerticalScrollIndicator={false}
-        w="100%"
-      />
-
-      <Button>Ask a question</Button>
-      {/* to be inside a modal */}
-      <>
-        <Text>Asking a question</Text>
-        <Input
-          mb="10"
-          placeholder="Please write your question here"
-          blurOnSubmit={true}
-          returnKeyType="done"
-          onSubmitEditing={() => {
-            Keyboard.dismiss()
-          }}
-          alignItems="center"
-          value={question}
-          onChangeText={(text) => {
-            setQuestion({
-              question: {
-                userId: user._id,
-                description: text,
-              },
-            })
-          }}
+      <View flex={0.5}>
+        <Carousel
+          data={product.images}
+          renderItem={carouselImages}
+          itemWidth={windowWidth}
+          sliderWidth={windowWidth}
+          onSnapToItem={(index) => setActiveSlide(index)}
         />
-        <Button onPress={handleQuestionSubmit}>Submit Question</Button>
-      </>
-      <ProductActionModal
+        <Text>{product.images?.length > 0 ? PaginationComponent(product.images) : null}</Text>
+      </View>
+
+      <ScrollView variant="wrapper" flex={0.5} mb={2}>
+        {product.fixedQandAs && QandAAccordions(product.fixedQandAs, 'fixed')}
+        {product.uniqQandAs && QandAAccordions(product.uniqQandAs, 'uniq')}
+
+        {/* to be inside a modal */}
+        <>
+          <Input
+            placeholder="Please write your question here"
+            blurOnSubmit={true}
+            returnKeyType="done"
+            onSubmitEditing={() => Keyboard.dismiss()}
+            alignItems="center"
+            value={question}
+            onChangeText={(text) => {
+              setQuestion({
+                question: {
+                  userId: user._id,
+                  description: text,
+                },
+              })
+            }}
+          />
+          <Button onPress={handleQuestionSubmit}>Question</Button>
+        </>
+      </ScrollView>
+
+      {/* <ProductActionModal
         modalHandler={modalHandler}
         modalVisible={isModalOpen}
         reportItem={reportItem}
         setReportItem={setReportItem}
-      />
+      /> */}
     </>
   )
 }
