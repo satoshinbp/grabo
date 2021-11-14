83ab8b85ef029aa07f334ab718e38053a84ebda7
Shinya Sato <0113shin.s@gmail.com>
2021-11-14 10:17:41 -0800

    fix args bug

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -12,8 +12,8 @@ const upload = multer({
   storage: multerS3({
     s3,
     bucket: 'grabo1',
-    metadata: (_, file, cb) => cb(null, { fieldName: file.fieldname }),
-    key: (_, _, cb) => cb(null, Date.now().toString()),
+    metadata: (req, file, cb) => cb(null, { fieldName: file.fieldname }),
+    key: (req, file, cb) => cb(null, Date.now().toString()),
   }),
 })
 
